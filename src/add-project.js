3e34d42293c2fda12ab4d21c95f02e21499b306e
John Eldrin Tolentino <115896724+jeldrint@users.noreply.github.com>
2023-03-20 13:21:59 +0800

    still working on add project feature

--- src/add-project.js
+++ src/add-project.js
@@ -1,7 +1,51 @@
-const addProject = () => {
+import { add } from 'date-fns';
+import Icon1 from './images/side-banner/icons-project.png';
+
+const addProject = () =>{
     const projectList = document.querySelector('#project-list');
     const projectListContainer = document.querySelector('#project-folder-content');
     const addProject = document.querySelector('#add-project');
+    
+    const addProjectPanel = () => {
+        const panelLogo = new Image();
+        panelLogo.src = Icon1;
+        panelLogo.style.width = '50px'
+        projectList.appendChild(panelLogo);
+
+        const panelDiv = document.createElement('div');
+        projectList.appendChild(panelDiv);
+
+        const panelInputBox = document.createElement('input');
+        panelInputBox.type = 'text';
+        panelInputBox.placeholder = 'Enter Project Name';
+        panelInputBox.style.padding = '5px';
+        panelDiv.className = 'panel-div'
+        panelDiv.appendChild(panelInputBox);
+
+
+        //BUTTONS
+        const panelAddButton = document.createElement('button');
+        panelAddButton.setAttribute('id','panel-add-button');
+        panelAddButton.className = 'panel-buttons'
+
+        const panelCancelButton = document.createElement('button');
+        panelCancelButton.setAttribute('id','panel-cancel-button');
+        panelAddButton.className = 'panel-buttons'
+
+        const panelButtons = document.createElement('div');
+
+        panelButtons.appendChild(panelAddButton);
+        panelButtons.appendChild(panelCancelButton);
+        panelDiv.appendChild(panelButtons);
+
+        panelAddButton.innerText = 'Add';
+        panelCancelButton.innerText = 'Cancel';
+
+    }
+
+    addProject.addEventListener('click', () =>{
+        addProjectPanel();
+    })
 }
 
 export default addProject;
--- src/body.js
+++ src/body.js
@@ -2,7 +2,6 @@ const layout = () => {
     const body = document.createElement('section');
     const container = document.querySelector('.container');
     
-    body.innerText = 'body';
     body.className = 'body-banner';
     container.appendChild(body);    
 }
